71389d9db732712479ec6538bc3e6c8eb566acd2
Kostas Milonas <milonas.ko@gmail.com>
2022-02-27 15:16:12 +0200

    adds repetitions

--- combo-announcer.py
+++ combo-announcer.py
@@ -2,18 +2,29 @@
 
 import random
 from subprocess import call
+import time
+
+max_punches = 3
+repetitions = 5
+speech_speed = 140
+wait_time = 4
 
 def random_int(limit=6):
     return random.randint(1, limit)
 
-total_punches = 3
+def main():
+    total_punches = random_int(max_punches)
 
-punches = []
-for i in range(total_punches):
-    punches.append(str(random_int()))
+    punches = []
+    for i in range(total_punches):
+        punches.append(str(random_int()))
 
-punches_string = ','.join(punches)
+    punches_string = ','.join(punches)
 
-print(punches_string)
-call(['espeak', '-s140', punches_string])
+    print(punches_string)
+    call(['espeak', '-s{}'.format(speech_speed), punches_string])
+    time.sleep(wait_time)
+
+for i in range(repetitions):
+    main()
 
