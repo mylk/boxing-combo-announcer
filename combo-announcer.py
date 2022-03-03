f279999b459f986739562f36f13e75dbd243c97f
Kostas Milonas <milonas.ko@gmail.com>
2022-03-03 10:27:39 +0200

    moves main code under __name__ condition

--- combo-announcer.py
+++ combo-announcer.py
@@ -5,13 +5,6 @@ import random
 from subprocess import call
 import time
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--max-punches', '-m', type=int, default=3, help='Maximum number of punches in a combo')
-parser.add_argument('--repetitions', '-r', type=int, default=5, help='Number of combos')
-parser.add_argument('--speech-speed', '-s', type=int, default=140, help='Speed of the announcer')
-parser.add_argument('--wait-time', '-w', type=int, default=4, help='Waiting time between combos')
-args = parser.parse_args()
-
 def random_int(limit=6):
     return random.randint(1, limit)
 
@@ -28,6 +21,14 @@ def main():
     call(['espeak', '-s{}'.format(args.speech_speed), punches_string])
     time.sleep(args.wait_time)
 
-for i in range(args.repetitions):
-    main()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--max-punches', '-m', type=int, default=3, help='Maximum number of punches in a combo')
+    parser.add_argument('--repetitions', '-r', type=int, default=5, help='Number of combos')
+    parser.add_argument('--speech-speed', '-s', type=int, default=140, help='Speed of the announcer')
+    parser.add_argument('--wait-time', '-w', type=int, default=4, help='Waiting time between combos')
+    args = parser.parse_args()
+
+    for i in range(args.repetitions):
+        main()
 
